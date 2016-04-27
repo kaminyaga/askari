59bbffeef6abc53b047c6f4e6a8d4b72fdc9a59e
andela-jkariuki <john.kariuki@andela.com>
2016-04-27 13:49:50 +0300

    Add procfile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-    Homepage
+    @if (url()->full() == route('homepage'))
+
+        <div id="landing-page" class="text-center">
+            <section class="landing-page-title">
+                <h1>Welcome to <strong class="site-highlight">Askari</strong></h1>
+                <h2>An online <strong class="site-highlight">Police management </strong> platform</h2>
+                <hr>
+                <div class="landing-page-call-to-action">
+                    <a href="{{ route('all_offenders') }}" class="btn btn-success btn-lg">GO TO DASHBOARD</a>
+                </div>
+            </section>
+
+        </div>
+
+    @endif
 @endsection
